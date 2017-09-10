7f13fceccdc83305ade0b5a17e03cdb568c492d9
DavideSaccardo <davide.saccardo14@gmail.com>
2017-09-10 21:56:23 +0200

    added for supremo

--- Project1/LU/LU.cpp
+++ Project1/LU/LU.cpp
@@ -9,17 +9,24 @@
 using namespace std;
 using namespace arma;
 
+ofstream ofile; /*print to file*/
+
 //function used
 double f(double x){return 100*exp(-10*x);};
 
 int main(int argc, char* argv[])
 {
 
-     int n=atoi(argv[1]);
-     if (argc<=1){
-        cout <<"You forgot something, watch out: you have to write the output file and n (int), size of vectors on same line." << endl;
-        exit(1);
-        }
+    int n=atoi(argv[1]);
+    int ii;
+    int q=atoi(argv[2]);
+    //mat max_err(q,3);
+    mat time(q,2);
+    if (argc<=1){
+             cout <<"You forgot something, watch out: you have to write the output file and n (int), size of vectors on same line." << endl;
+             exit(1);
+             }
+     for(ii=0;ii<=q-1;ii++){
 
     //Declaration and allocation of variables with Armadillo
     mat A=zeros<mat>(n+2,n+2);
@@ -32,8 +39,8 @@ int main(int argc, char* argv[])
     int i,j; //indexes
 
     //time calculation
-    //    clock_t t[n];
-    //    t[n]=clock();
+        clock_t t;
+        t=clock();
 
     //filling the tridiagonal matrix A
     for(i=1;i<=n+1;i++){
@@ -58,13 +65,31 @@ int main(int argc, char* argv[])
 
     //vec v=solve(A,r); /*numerical solution with LU, and I print it*/
     //cout << v << endl;
-    cout <<A*C<< endl;
-mat B=A.i();
-B.print();
 
-    // Finishing clock and printing the time needed
-    //    t[n]=(float) (clock()-t[n])/CLOCKS_PER_SEC;
-    //    cout << "the programme took " << t[n]<< " seconds" << endl;
+//    // find LU decomp of A, if needed, P is the permutation matrix
+//      mat L, U;
+//      lu(L,U,A);
+//      // print l
+//      L.print(" L= ");
+//      // print U
+//      U.print(" U= ");
+//      //Check that A = LU
+//      (A-L*U).print("Test of LU decomposition");
 
+    time(ii,0)=n;
+    time(ii,1)=(float) (clock()-t)/CLOCKS_PER_SEC;
+
+   n=n*10;
+     }
+     // Open file and write results to file:
+     ofile.open("time_LU.txt");
+     ofile << setiosflags(ios::showpoint | ios::uppercase);
+     ofile << " n:          time: " << endl;
+     for (int i=0;i<=q-1;i++) {
+     ofile << setw(15) << setprecision(8) << time(i,0);
+     ofile << setw(15) << setprecision(8) << time(i,1) << endl;
+     }
+  ofile.close();
+time.print();
     return 0;
 }
